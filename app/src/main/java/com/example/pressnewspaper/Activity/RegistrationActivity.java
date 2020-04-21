ff6b1febf8d79a2181fc9cf7864ef7216ec8e016
Altayeb Omer <altayeb3mer@gmail.com>
2020-04-21 15:27:28 -0700

    aaa

---
+++ app/src/main/java/com/example/pressnewspaper/Activity/RegistrationActivity.java
@@ -0,0 +1,16 @@
+package com.example.pressnewspaper.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.pressnewspaper.R;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
