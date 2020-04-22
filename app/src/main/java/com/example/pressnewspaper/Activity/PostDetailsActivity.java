ea1452dafb5cb7ad7fb04ca180f6ca10b9f0e3f3
Altayeb Omer <altayeb3mer@gmail.com>
2020-04-22 14:47:10 -0700

    22-4-2020

---
+++ app/src/main/java/com/example/pressnewspaper/Activity/PostDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.example.pressnewspaper.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.pressnewspaper.R;
+
+public class PostDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_details);
+    }
+}
