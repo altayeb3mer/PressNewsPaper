535dd3283049a7546ab4eb3f3d88de5f1854b14e
Altayeb Omer <altayeb3mer@gmail.com>
2020-09-13 06:56:26 -0700

    30-8-2020 12:50 AM

---
+++ app/src/main/java/com/example/pressnewspaper/Activity/MySubDetails.java
@@ -0,0 +1,16 @@
+package com.example.pressnewspaper.Activity;
+
+import android.os.Bundle;
+
+import com.example.pressnewspaper.R;
+import com.example.pressnewspaper.Utils.ToolbarClass;
+
+public class MySubDetails extends ToolbarClass {
+
+    protected final void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.onCreate(R.layout.activity_my_sub_details, "تفاصيل الاشتراك");
+    }
+
+
+}
